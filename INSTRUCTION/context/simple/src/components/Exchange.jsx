d8611dc73bb208ccb9f877f2692116c40b2fc16a
fatih <akkaya064@gmail.com>
2023-09-29 16:27:41 +0100

    Currency

---
+++ INSTRUCTION/context/simple/src/components/Exchange.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from './Content'
+import TopBar from './TopBar'
+
+const Exchange = () => {
+  return (
+    <>
+      <TopBar/> 
+      <Content />
+      </>
+  )
+}
+
+export default Exchange
