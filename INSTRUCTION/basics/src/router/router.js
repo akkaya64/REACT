f91e44634211cc8c3ce2a588e60461033dbb6f48
fatih <akkaya064@gmail.com>
2023-09-29 10:17:34 +0100

    React Router

--- INSTRUCTION/basics/src/router/router.js
+++ INSTRUCTION/basics/src/router/router.js
@@ -46,6 +46,8 @@ import Form05 from "../components/24-forms/05-Form";
 import FormikYub01 from "../components/25-formik-yup/FormikYub01";
 import FormikYup02 from "../components/25-formik-yup/FormikYup02";
 import { Bananas, Pears } from "../components/01-my-house/01-my-house";
+import Users from "../components/26-react-router/users";
+import UserDetails from "../components/26-react-router/user-details";
 //import '../../src/'
 
 export const router = createBrowserRouter([
@@ -58,17 +60,17 @@ export const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'x',
+        path: "x",
         //element: <X/>
-    },
-    {
-        path: 'pears',
-        element: <Pears />
-    },
-    {
-        path: 'bananas',
-        element: <Bananas />
-    },
+      },
+      {
+        path: "pears",
+        element: <Pears />,
+      },
+      {
+        path: "bananas",
+        element: <Bananas />,
+      },
       {
         path: "jsx",
         children: [
@@ -172,207 +174,219 @@ export const router = createBrowserRouter([
             element: <Image style={{ width: "100%", height: "auto" }} />,
           },
           {
-            path: 'gallery',
-            element: <ImageGallery/>
-          }
+            path: "gallery",
+            element: <ImageGallery />,
+          },
         ],
       },
       {
-        path: 'profile-card',
-        element: <ProfileCard
+        path: "profile-card",
+        element: (
+          <ProfileCard
             src="profile.jpg"
             name="Zisan"
             location="Istanbul, Turkey"
-            statistics={
-                [
-                    {
-                        stat: 8,
-                        title: "Shot"
-                    },
-                    {
-                        stat: 1000,
-                        title: "Followers"
-                    },
-                    {
-                        stat: 50,
-                        title: "Following"
-                    }
-                ]
-            }
-        />
-    },
-    {
-        path: 'bootstrap',
+            statistics={[
+              {
+                stat: 8,
+                title: "Shot",
+              },
+              {
+                stat: 1000,
+                title: "Followers",
+              },
+              {
+                stat: 50,
+                title: "Following",
+              },
+            ]}
+          />
+        ),
+      },
+      {
+        path: "bootstrap",
         children: [
-            {
-                index: true,
-                element: <BootstrapKlasikYontem />
-            },
-            {
-                path: 'klasik-yontem',
-                element: <BootstrapKlasikYontem />
-            },
-            {
-                path: 'react-bootstrap',
-                element: <BootstrapComponents />
-            },
-            {
-                path: 'button',
-                element: <BootstrapButtons variant="outline-primary" disabled />
-
-            }
-        ]
-    },
-    {
-        path: 'icons',
+          {
+            index: true,
+            element: <BootstrapKlasikYontem />,
+          },
+          {
+            path: "klasik-yontem",
+            element: <BootstrapKlasikYontem />,
+          },
+          {
+            path: "react-bootstrap",
+            element: <BootstrapComponents />,
+          },
+          {
+            path: "button",
+            element: <BootstrapButtons variant="outline-primary" disabled />,
+          },
+        ],
+      },
+      {
+        path: "icons",
         children: [
-            {
-                index: true,
-                element: <ReactIcons />
-            },
-            {
-                path: 'react-icons',
-                element: <ReactIcons />
-            },
-            {
-                path: 'material-icons',
-                element: <MaterialIcons />
-            }
-        ]
-    },
-    {
-        path: 'events',
-        element: <Events />
-    },
-    {
-        path: 'product-shop',
-        element: <ProductShop/>
-    },
-    {
-        path: 'state',
-        element: <State />
-    },
-    {
-        path: 'counter',
-        element: <Counter />
-    },
-    {
-        path: 'birthday',
-        element: <Birthday />
-    },
-    {
-        path: 'useeffect',
-        element: <UseEffect />
-    },
-    {
-        path: 'counter-title',
-        element: <CounterTitle />
-    },
-    {
-        path: 'useref',
-        element: <UseRef />
-    },
-    {
-        path: 'component-types',
+          {
+            index: true,
+            element: <ReactIcons />,
+          },
+          {
+            path: "react-icons",
+            element: <ReactIcons />,
+          },
+          {
+            path: "material-icons",
+            element: <MaterialIcons />,
+          },
+        ],
+      },
+      {
+        path: "events",
+        element: <Events />,
+      },
+      {
+        path: "product-shop",
+        element: <ProductShop />,
+      },
+      {
+        path: "state",
+        element: <State />,
+      },
+      {
+        path: "counter",
+        element: <Counter />,
+      },
+      {
+        path: "birthday",
+        element: <Birthday />,
+      },
+      {
+        path: "useeffect",
+        element: <UseEffect />,
+      },
+      {
+        path: "counter-title",
+        element: <CounterTitle />,
+      },
+      {
+        path: "useref",
+        element: <UseRef />,
+      },
+      {
+        path: "component-types",
         children: [
-            {
-                index: true,
-                element: <ClassTypeComponent />
-            },
-            {
-                path: 'class-type-component',
-                element: <ClassTypeComponent />
-            },
-            {
-                path: 'function-type-component',
-                element: <FunctionTypeComponent />
-            }
-        ]
-    },
-    {
-        path: 'fetch',
+          {
+            index: true,
+            element: <ClassTypeComponent />,
+          },
+          {
+            path: "class-type-component",
+            element: <ClassTypeComponent />,
+          },
+          {
+            path: "function-type-component",
+            element: <FunctionTypeComponent />,
+          },
+        ],
+      },
+      {
+        path: "fetch",
         children: [
-            {
-                index: true,
-                element: <Fetch1 />
-            },
-            {
-                path: '1',
-                element: <Fetch1 />
-            },
-            {
-                path: '2',
-                element: <Fetch02 />
-            }
-        ]
-    },
-    {
-        path: 'countries',
-        element: <Countries />
-    },
-    {
-        path: 'parent-component',
-        element: <ParentComponent />
-    },
-    {
-        path: 'form',
+          {
+            index: true,
+            element: <Fetch1 />,
+          },
+          {
+            path: "1",
+            element: <Fetch1 />,
+          },
+          {
+            path: "2",
+            element: <Fetch02 />,
+          },
+        ],
+      },
+      {
+        path: "countries",
+        element: <Countries />,
+      },
+      {
+        path: "parent-component",
+        element: <ParentComponent />,
+      },
+      {
+        path: "form",
         children: [
-            {
+          {
+            index: true,
+            element: <Form />,
+          },
+          {
+            path: "1",
+            element: <Form />,
+          },
+          {
+            path: "2",
+            element: <Form02 />,
+          },
+          {
+            path: "3",
+            element: <Form03 />,
+          },
+          {
+            path: "4",
+            element: <Form04 />,
+          },
+          {
+            path: "5",
+            children: [
+              {
                 index: true,
-                element: <Form />
-            },
-            {
-                path: '1',
-                element: <Form />
-            },
-            {
-                path: '2',
-                element: <Form02 />
-            },
-            {
-                path: '3',
-                element: <Form03 />
-            },
-            {
-                path: '4',
-                element: <Form04 />
-            },
-            {
-                path: '5',
-                children: [
-                    {
-                        index: true,
-                        element: <Form05 />
-                    },
-                    {
-                        path: ':formId',
-                        element: <Form05 />
-                    }
-                ]
-            },
-        ]
-    },
-    {
-        path: 'formik-yup',
+                element: <Form05 />,
+              },
+              {
+                path: ":formId",
+                element: <Form05 />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        path: "formik-yup",
         children: [
-            {
-                index: true,
-                element: <FormikYub01 />
-            },
-            {
-                path: '1',
-                element: <FormikYub01 />
-            },
-            {
-                path: '2',
-                element: <FormikYup02 />
-            }
-        ]
-    },
+          {
+            index: true,
+            element: <FormikYub01 />,
+          },
+          {
+            path: "1",
+            element: <FormikYub01 />,
+          },
+          {
+            path: "2",
+            element: <FormikYup02 />,
+          },
+        ],
+      },
+      {
+        path: "users",
+        children: [
+          {
+            index: true,
+            element: <Users />
+          },
+          {
+            path: ":userId",
+            element: <UserDetails />
+          },
+        ],
+      },
     ],
   },
   {
-    path: '*',
-    element: <h1>404 Not Found</h1>
-}
+    path: "*",
+    element: <h1>404 Not Found</h1>,
+  },
 ]);
